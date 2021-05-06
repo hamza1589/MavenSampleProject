f44dfec59db687e938bb3c066d2bce1d727c8ca9
hamza <hamzacherfaoui32@yahoo.com>
2021-05-06 00:25:45 -0400

    commit

---
+++ src/test/java/QAclickAcademy/AppTest.java
@@ -0,0 +1,4 @@
+package QAclickAcademy;
+
+public class AppTest {
+}
---
+++ src/test/java/QAclickAcademy/AppiumTest.java
@@ -0,0 +1,15 @@
+package QAclickAcademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+    @Test
+    public void nativeAppAndroid(){
+        System.out.println("nativeAppAndroid");
+
+    }
+    @Test
+    public void IOSApp(){
+        System.out.println("IOSApp");
+    }
+}
